d3b2f3e1462a15974d0b332f33aced19263302af
AbdelMonaam Aouini <monaam.aouini@retraced.com>
2023-02-25 18:43:18 +0100

    move to vite bundler

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 3500,
+  },
+  preview: {
+    port: 3500,
+  },
+
+  cacheDir: ".vite",
+  build: {
+    target: "esnext",
+  },
+});
