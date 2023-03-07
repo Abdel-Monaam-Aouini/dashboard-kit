e914d9403a033123b07e97f7b11d2a8d0130ec16
Abdel-Monaam-Aouini <clemonaam@gmail.com>
2023-03-07 13:45:59 +0100

    feat: add semantic release config

---
+++ release.config.js
@@ -0,0 +1,15 @@
+const config = {
+  brnaches: ["master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/git",
+    {
+      message:
+        "chore(release): ${nextRelease.version} [skip-ci]\n\n${nextRelease.notes}",
+    },
+    "@semantic-release/github",
+  ],
+};
+
+module.exports = config;
