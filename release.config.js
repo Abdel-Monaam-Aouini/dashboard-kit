d4371baee93c9d1285dc4e330d9fbe5bfb920e5f
Abdel-Monaam-Aouini <clemonaam@gmail.com>
2023-03-17 19:21:38 +0100

    fix: release confi

--- release.config.js
+++ release.config.js
@@ -1,27 +1,28 @@
-{
-  "branches": ["master"],
-  "plugins": [
+module.exports = {
+  branches: ["master"],
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
       {
-        "changelogFile": "CHANGELOG.md"
-      }
+        changelogFile: "CHANGELOG.md",
+      },
     ],
     "@semantic-release/npm",
     "@semantic-release/github",
     [
       "@semantic-release/git",
       {
-        "assets": [
+        assets: [
           "CHANGELOG.md",
           "dist/**",
           "package.json",
-          "package-lock.json"
+          "package-lock.json",
         ],
-        "message": "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
-    ]
-  ]
-}
+        message:
+          "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
